3c27f4941aea147b8b2ca882db2b118354f4a170
Ziva Vatra <zv@ziva-vatra.com>
2016-11-23 22:20:25 +0000

    Cleanup to be more compliant with PEP8, bugfixes as well

--- aac.py
+++ aac.py
@@ -5,7 +5,8 @@ import os
 import sys
 from shell import shell
 from flac import flac, flacdecode
-from config import *
+from config import ipaths
+from time import time
 
 # This is for the open source implementation. In this case we went for the
 # Open Source Fraunhofer AAC Encoder (fdk-aac)
@@ -14,125 +15,142 @@ from config import *
 class aacplus(object):
     def __init__(self, aacopts):
         self.opts = aacopts
-        if os.path.exists("%saac-enc" % aacpath) is False:
+        if os.path.exists("%saac-enc" % ipaths.aacpath) is False:
             print "Error: %saac-enc not found (is fdk-aac installed?)\
-            Cannot convert" % aacpath
+            Cannot convert" % ipaths.aacpath
             sys.exit(-1)
 
-    def AACPconvert(self,infile,outfile,logq):
-        inmetadata = flac().getflacmeta(infile)
-        decoder = flacdecode(infile,outfile)()
+    def AACPconvert(self, infile, outfile, logq):
+        decoder = flacdecode(infile, outfile)()
         encoder = os.popen("%saac-enc %s - \"%s.aac\" > /tmp/aacplusLog" % (
-            aacpath,
+            ipaths.aacpath,
             self.opts,
             outfile,
-            ) ,'wb',8192)
+        ), 'wb', 8192
+        )
 
-        for line in decoder.readlines(): #while data exists in the decoders buffer
-            encoder.write(line) #write it to the encoders buffer
+        # while data exists in the decoders buffer
+        for line in decoder.readlines():
+            encoder.write(line)  # write it to the encoders buffer
 
-        decoder.flush() #if there is any data left in the buffer, clear it
-        decoder.close() #somewhat self explanetory
+        decoder.flush()  # if there is any data left in the buffer, clear it
+        decoder.close()  # somewhat self explanetory
 
-        encoder.flush() #as above
+        encoder.flush()  # as above
         encoder.close()
 
 
-
-#For the binary-only Nero AAC encoder
-class aacplusNero:
+# For the binary-only Nero AAC encoder
+class aacplusNero(object):
     def __init__(self, aacopts):
         self.opts = aacopts
-        if os.path.exists("%sneroAacEnc" % neroaacpath) == False:
+        if not os.path.exists("%sneroAacEnc" % ipaths.neropath):
             print "ERROR: NeroAacEnc not found! Cannot convert."
             sys.exit(-1)
 
-    def generateNeroTags(self,indata):
-        ''' The new versions of nero AAC encoder for Linux provides neroAacTag '''
+    def generateNeroTags(self, indata):
+        '''
+        The new versions of nero AAC encoder for Linux provides neroAacTag
+        '''
         tags = ""
 
-        #NeroTag format (for 'standard Nero Digital') along with 'indata' keys
-            #  title = TITLE
-            #  artist = ARTIST
-            #  year = DATE
-            #  album = ALBUM
-            #  genre = GENERE
-            #  track = TRACKNUMBER
-            #  totaltracks = 
-            #  disc
-            #  totaldiscs
-            #  url = URL
-            #  copyright = PUBLISHER
-            #  comment = COMMENT (if blank, put in something like 'converted by flac2all ($url)' )
-            #  lyrics
-            #  credits = ENCODEDBY
-            #  rating
-            #  label = PUBLISHER
-            #  composer = COMPOSER
-            #  isrc
-            #  mood
-            #  tempo
-        #  In format: -meta:<name>=<value> 
-        #  User-defined fields are -meta-user:<name>=<value> 
+        # NeroTag format (for 'standard Nero Digital') along with 'indata' keys
+        #  title = TITLE
+        #  artist = ARTIST
+        #  year = DATE
+        #  album = ALBUM
+        #  genre = GENERE
+        #  track = TRACKNUMBER
+        #  totaltracks =
+        #  disc
+        #  totaldiscs
+        #  url = URL
+        #  copyright = PUBLISHER
+        #  comment = COMMENT (if blank, put in something like
+        #        'converted by flac2all ($url)' )
+        #  lyrics
+        #  credits = ENCODEDBY
+        #  rating
+        #  label = PUBLISHER
+        #  composer = COMPOSER
+        #  isrc
+        #  mood
+        #  tempo
+        # In format: -meta:<name>=<value>
+        # User-defined fields are -meta-user:<name>=<value>
 
         validnerotags = {
-            'title':'TITLE',
-            'artist':'ARTIST',
-            'year':'DATE',
-            'album':'ALBUM',
-            'genre':'GENRE',
-            'track':'TRACKNUMBER',
-            'copyright':'PUBLISHER',
-            'comment':'COMMENT',
-            'credits':'ENCODEDBY',
-            'label':'PUBLISHER',
-            'composer':'COMPOSER',
-            'url':'URL',
+            'title': 'TITLE',
+            'artist': 'ARTIST',
+            'year': 'DATE',
+            'album': 'ALBUM',
+            'genre': 'GENRE',
+            'track': 'TRACKNUMBER',
+            'copyright': 'PUBLISHER',
+            'comment': 'COMMENT',
+            'credits': 'ENCODEDBY',
+            'label': 'PUBLISHER',
+            'composer': 'COMPOSER',
+            'url': 'URL',
         }
 
         for nerokey in validnerotags:
             try:
-                tag = indata[ validnerotags[nerokey] ] 
+                tag = indata[validnerotags[nerokey]]
             except KeyError as e:
                 if e.message == 'COMMENT':
-                    tag = " converted by flac2all  - http://code.google.com/p/flac2all/ "
+                    tag = "Converted by flac2all,\
+                    https://github.com/ZivaVatra/flac2all/"
                 else:
                     continue
-            tags += " -meta:\"%s\"=\"%s\" " % (nerokey.strip(), tag.strip() )
+            tags += " -meta:\"%s\"=\"%s\" " % (nerokey.strip(), tag.strip())
 
         return tags
 
-    def AACPconvert(self,infile,outfile,logq):
-
+    def AACPconvert(self, infile, outfile, logq):
+        startTime = time()
         inmetadata = flac().getflacmeta("\"" + infile + "\"")
 
-        tagcmd = "%sneroAacTag " % neroaacpath
+        tagcmd = "%sneroAacTag " % ipaths.neropath
         try:
             metastring = self.generateNeroTags(inmetadata)
         except(UnboundLocalError):
             metastring = ""
 
         decoder = flacdecode(infile)()
-        #wb stands for write-binary
-        encoder = os.popen("%sneroAacEnc %s -if - -of %s.mp4 > /tmp/aacplusLog" % (
-            neroaacpath,
+        encoder = os.popen("%sneroAacEnc %s -if - -of %s.mp4 >/tmp/neroLog" % (
+            ipaths.neropath,
             self.opts,
             shell().parseEscapechars(outfile),
-            ) ,'wb',8192)
+        ), 'wb', 8192)
 
-        for line in decoder.readlines(): #while data exists in the decoders buffer
-            encoder.write(line) #write it to the encoders buffer
+        # while data exists in the decoders buffer
+        for line in decoder.readlines():
+            encoder.write(line)  # write it to the encoders buffer
 
-        decoder.flush() #if there is any data left in the buffer, clear it
-        decoder.close() #somewhat self explanetory
+        decoder.flush()  # if there is any data left in the buffer, clear it
+        decoder.close()  # somewhat self explanetory
 
-        encoder.flush() #as above
+        encoder.flush()
         encoder.close()
 
-        #Now as the final event, load up the tags
+        # Now as the final event, load up the tags
         rc = os.system("%s \"%s.mp4\" %s" % (tagcmd, outfile, metastring))
-#        print "%s %s.mp4 %s" % (tagcmd, outfile, metastring)
         if rc != 0:
-            logq.put([infile,outfile,"aacNero","WARNING: Could not tag AAC file",rc, time() - startTime])
+            logq.put([
+                infile,
+                outfile,
+                "aacNero",
+                "WARNING: Could not tag AAC file",
+                rc,
+                time() - startTime
+            ])
         else:
-            logq.put([infile,outfile,"aacNero","SUCCESS",0, time() - startTime])
+            logq.put([
+                infile,
+                outfile,
+                "aacNero",
+                "SUCCESS",
+                0,
+                time() - startTime
+            ])
