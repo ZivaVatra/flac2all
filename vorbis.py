ff33e96bfa4c766d65321a779b011e8690d44260
Ziva Vatra <zv@ziva-vatra.com>
2016-11-25 21:29:17 +0000

    Refactored vorbis code, tested working with subprocess module

--- vorbis.py
+++ vorbis.py
@@ -1,14 +1,15 @@
 # vim: ts=4 autoindent expandtab
 from config import ipath
-import os
-from shell import shell
 from time import time
+import subprocess as sp
 
 
 # Class that deals with vorbis
 class vorbis:
     def __init__(self, vorbis_options):
-            self.opts = vorbis_options
+            self.opts = filter(
+                lambda x: x.strip() != "", vorbis_options.split(' ')
+            )
 
     def oggconvert(self, infile, outfile, logq):
         # oggenc automatically parses the flac file + metadata, quite wonderful
@@ -16,17 +17,23 @@ class vorbis:
         # The binary itself deals with the tag conversion etc
         # Which makes our life rather easy
         startTime = time()
-        rc = os.system("%soggenc %s -Q -o %s.ogg %s" % (
-            ipath.oggencpath,
-            self.opts,
-            shell().parseEscapechars(outfile),
-            shell().parseEscapechars(infile)
-        ))
-
-        if rc == 0:
-            result = "SUCCESS"
+        cmd = [
+            "%soggenc" % ipath.oggencpath,
+            "-Q",
+            "-o",
+            "%s.ogg" % outfile,
+        ]
+        if len(self.opts) != 0:
+            cmd.extend(self.opts)
+        cmd.append(infile)
+        rc = -1
+        try:
+            rc = sp.check_call(cmd)
+        except sp.CalledProcessError as e:
+            result = "ERROR:oggenc %s" % e.message
         else:
-            result = "ERROR:oggenc"
+            result = "SUCCESS"
+
         logq.put([
             infile,
             outfile,
