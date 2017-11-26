5e7b20695cad5088401b882876f9c54fe1c0f391
ZV <zv@ziva-vatra.com>
2017-11-26 10:28:48 +0100

    Refinement of setup.py for eventual inclusion into pypi

--- setup.py
+++ setup.py
@@ -10,17 +10,49 @@
 #  =============================================================|
 
 from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+long_description = '''
+Started in 2003, flac2All has grown into a multi-process script that will
+convert your collection of FLAC files into various other formats (currently
+mp3,ogg vorbis, opus, flac and acc), complete with any tags that the source
+file had. Designed to be extended with new formats easily as time goes on,
+it is a utility for people with with large FLAC collections who want a way
+to convert multiple files in parallel.
+
+Tested on Linux and FreeBSD, let me know how it works on other Unix OS'es.
+'''
+
+
 setup(
     name="flac2all",
     version="4.0",
     packages=find_packages(),
+
+    # metadata for upload to PyPI
+    author="ZivaVatra",
+    author_email="zv@ziva-vatra.com",
+    description='''
+Multi process,multi codec audio converter with tagging support
+''',
+    license="GPLv3",
+    url="http://ziva-vatra.com/index.php?aid=23",
+    # From https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: BSD',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Multimedia :: Sound/Audio :: Conversion',
+    ],
+    keywords='multithread, multiprocess, batch, flac, converter, mp3, vorbis,\
+opus, aac, lame, music, audio',
+
 )
-
-
-# metadata for upload to PyPI
-author = "ZivaVatra",
-author_email = "zv@ziva-vatra.com",
-description = "Started in 2003, flac2All has grown into a multi-process script that will convert your collection of FLAC files into various other formats (currently mp3,ogg vorbis, opus, flac and acc), complete with any tags that the source file had. Designed to be extended with new formats easily as time goes on, it is a utility for people with with large FLAC collections who also want a way to convert multiple files in parallel.",
-license = "GPLv3",
-keywords = "multithread multiprocess batch flac converter mp3 vorbis opus aac lame",
-url = "http://ziva-vatra.com/index.php?aid=23",   # project home page, if any
