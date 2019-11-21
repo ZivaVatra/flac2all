8c98ae6dba66e89181f6a621afcb20fa5ece36dc
zv <info@ziva-vatra.com>
2019-11-21 17:00:24 +0000

    Initial import

--- flac2all_pkg/logging.py
+++ flac2all_pkg/logging.py
@@ -2,8 +2,43 @@
 
 import datetime
 import sys
+from ccons import cursecons
 
 from termcolor import cprint
+
+
+class cconsole(cursecons):
+	def __init__(self):
+		cursecons.__init__(self)
+		self.messagelines = []
+		self.workers = None
+		self.complete = 0
+		self.error = 0
+		self.tasks = 0
+
+	def _msg_display(self, status, msg):
+		msg = msg.encode("utf-8", "replace").decode()
+		msg = "%s: %s" % (
+			datetime.datetime.utcnow().strftime("UTC~%H:%M:%S"),
+			msg
+		)
+		self.messagelines.append([status, msg])
+		self.message_box(self.messagelines)
+
+	def status(self, msg):
+		self._msg_display(["status", msg])
+
+	def info(self, msg):
+		self._msg_display(["info", msg])
+
+	def ok(self, msg):
+		self._msg_display(["ok", msg])
+
+	def warn(self, msg):
+		self._msg_display(["warn", msg])
+
+	def crit(self, msg):
+		self._msg_display(["crit", msg])
 
 
 class console():
