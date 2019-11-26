1edf411fa8109aec76e0390754868acefb165417
zv <info@ziva-vatra.com>
2019-11-26 17:40:28 +0000

    bugfix: issues with message window overflow

--- flac2all_pkg/ccons.py
+++ flac2all_pkg/ccons.py
@@ -85,7 +85,7 @@ class cursecons:
 
 	def message_box(self, messages):
 		height = self.winheight - 8
-		msg = messages[-(height + 15):]  # Show the last x messages we can fit
+		msg = messages[-10:]  # Show the last x messages we can fit
 		win = self.window(height, self.winwidth, 8, 0, "Messages", True)
 		x = 1
 		for line in msg:
